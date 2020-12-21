9acc4bccef63ac2c9fea1683d1e5c71ff9b15bb0
xia_feng_global <xia_feng_global@qq.com>
2020-12-21 21:08:10 +0800

    eidt by hot_Fix

--- atcrowdfunding04-admin-entity/src/main/java/com/atguigu/crowd/entity/Address.java
+++ atcrowdfunding04-admin-entity/src/main/java/com/atguigu/crowd/entity/Address.java
@@ -6,6 +6,43 @@ public class Address {
 	private String city;
 	private String street;
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((province == null) ? 0 : province.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (province == null) {
+			if (other.province != null)
+				return false;
+		} else if (!province.equals(other.province))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
 	public Address() {
 		
 	}
